81bfe64a61cc1796fdd24f1e08ccc5ac2b8638e2
Ludwig Magnusson <ludwig@mediatool.com>
2021-03-02 07:15:51 +0100

    feat: add fixing capabilities to verify-paths

--- scripts/verify-paths.js
+++ scripts/verify-paths.js
@@ -1,9 +1,15 @@
-const { readFileSync } = require('fs')
+const { readFileSync, writeFileSync } = require('fs')
 const { deepStrictEqual } = require('assert')
 
-const expectedPaths = [ '- node_modules', ...process.argv.slice(2).map((s) => `- modules/${s}/node_modules`) ]
+const argv = process.argv.slice(2)
+const subpaths = argv.filter((arg) => arg !== '--fix')
 
-const content = readFileSync(`${__dirname}/../.circleci/config.yml`, 'utf-8')
+const fix = argv.length !== subpaths.length
+
+const expectedPaths = [ '- node_modules', ...subpaths.map((s) => `- modules/${s}/node_modules`) ]
+
+const filePath = `${__dirname}/../.circleci/config.yml`
+const content = readFileSync(filePath, 'utf-8')
 
 function matches (regex) {
   return (string) => regex.test(string)
@@ -13,7 +19,7 @@ function trim (s) {
   return s.trim()
 }
 
-const lines = content.split('\n').map(trim)
+const lines = content.split('\n')
 const firstLine = lines.findIndex(matches(/#pathsbegin/)) + 1
 const lastLine = lines.findIndex(matches(/#pathsend/))
 
@@ -31,9 +37,43 @@ if (lastLine === -1) {
   sendError('Could not find #pathsend marker in circle ci config file')
 }
 
-const actualPaths = lines.slice(firstLine, lastLine)
+function createPadding (n, string = '') {
+  return string.length < n
+    ? createPadding(n, ` ${string}`)
+    : string
+}
+
+function fixFile () {
+  const firstLineContent = lines[firstLine]
+  const paddingSize = firstLineContent.split('').findIndex((c) => c !== ' ')
+  const padding = createPadding(paddingSize)
+  const paddedLines = expectedPaths.map((p) => `${padding}${p}`)
+  const nbrLines = lastLine - firstLine
+  lines.splice(firstLine, nbrLines, ...paddedLines)
+  writeFileSync(filePath, lines.join('\n'))
+}
+
+const actualPaths = lines.slice(firstLine, lastLine).map(trim)
+
+function niceError () {
+  const notFound = expectedPaths.filter((p) => actualPaths.indexOf(p) === -1)
+  const extra = actualPaths.filter((p) => expectedPaths.indexOf(p) === -1)
+  const errorLines = [ 'Cache paths not matching expected paths:' ]
+  if (notFound.length > 0) {
+    errorLines.push('Expected modules not found:', ...notFound)
+  }
+  if (extra.length > 0) {
+    errorLines.push('Unexpected modules in conf file:', ...extra)
+  }
+  return errorLines.join('\n')
+}
+
 try {
   deepStrictEqual(actualPaths, expectedPaths)
 } catch (e) {
-  sendError(`Cache paths not matching expected paths:\nActual:\n${actualPaths}\nExpected:\n${expectedPaths}`)
+  if (fix) {
+    fixFile()
+  } else {
+    sendError(niceError())
+  }
 }
